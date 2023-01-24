de2594e9ba5e6d24ad25f9aa65fad85b8ef781e5
Benjamin Sims <arycama@gmail.com>
2023-01-24 23:08:15 +1030

    derivative adjustment

--- ShaderLibrary/Resources/Common.hlsl
+++ ShaderLibrary/Resources/Common.hlsl
@@ -31,40 +31,24 @@ float GetHeight(int x, int y)
 
 void GetDerivatives(int2 id, float3 Scale, out float2 d0, out float3 d1)
 {
-	float z1 = GetHeight(id.x - 1, id.y + 1);
-    float z2 = GetHeight(id.x + 0, id.y + 1);
-    float z3 = GetHeight(id.x + 1, id.y + 1);
-    float z4 = GetHeight(id.x - 1, id.y + 0);
-    float z5 = GetHeight(id.x + 0, id.y + 0);
-    float z6 = GetHeight(id.x + 1, id.y + 0);
-    float z7 = GetHeight(id.x - 1, id.y - 1);
-    float z8 = GetHeight(id.x + 0, id.y - 1);
-    float z9 = GetHeight(id.x + 1, id.y - 1);
+	float tl = GetHeight(id.x - 1, id.y + 1);
+    float t = GetHeight(id.x + 0, id.y + 1);
+    float tr = GetHeight(id.x + 1, id.y + 1);
+    float l = GetHeight(id.x - 1, id.y + 0);
+    float c = GetHeight(id.x + 0, id.y + 0);
+    float r = GetHeight(id.x + 1, id.y + 0);
+    float bl = GetHeight(id.x - 1, id.y - 1);
+    float b = GetHeight(id.x + 0, id.y - 1);
+    float br = GetHeight(id.x + 1, id.y - 1);
 
     //p, q
-	d0.x = (z1 + 2.0 * z4 + z7 - z3 - 2.0 * z6 - z9) / (8.0 * Scale.x);
-	d0.y = (z1 + 2.0 * z2 + z3 - z7 - 2.0 * z8 - z9) / (8.0 * Scale.z);
+	d0.x = (r - l) / (2.0 * Scale.x);
+	d0.y = (t - b) / (2.0 * Scale.z);
 
     //r, t, s
-	d1.x = (-z1 + 2.0 * z2 - z3 - 2.0 * z4 + 4.0 * z5 - 2.0 * z6 - z7 + 2.0 * z8 - z9) / (4.0 * Scale.x * Scale.x);
-	d1.y = (-z1 - 2.0 * z2 - z3 + 2.0 * z4 + 4.0 * z5 + 2.0 * z6 - z7 - 2.0 * z8 - z9) / (4.0 * Scale.z * Scale.z);
-    d1.z = (-z1 + z3 + z7 - z9) / (4.0 * Scale.x * Scale.z);
-
-	// Evans young
-	//d0.x = (h[2][2] + h[2][1] + h[2][0] - h[0][2] - h[0][1] - h[0][0]) / (6.0 * Scale.x);
-	//d0.y = (h[0][2] + h[1][2] + h[2][2] - h[0][0] - h[1][0] - h[2][0]) / (6.0 * Scale.z);
-
-	//d1.x = (h[0][2] + h[2][2] + h[0][1] + h[2][1] + h[0][0] + h[2][0] - 2.0 * (h[1][2] + h[1][1] + h[1][0])) / (3.0 * Scale.x * Scale.x);
-	//d1.y = (h[0][2] + h[1][2] + h[2][2] + h[0][0] + h[1][0] + h[2][0] - 2.0 * (h[0][1] + h[1][1] + h[2][1])) / (3.0 * Scale.z * Scale.z);
-	//d1.z = (h[2][2] + h[0][0] - h[0][2] - h[2][0]) / (4.0 * Scale.x * Scale.z);
-
-	// Sobel
-	//d0.x = ((h[2][0] + 2.0 * h[2][1] + h[2][2]) - (h[0][0] + 2.0 * h[0][1] + h[0][2])) / (8.0 * Scale.x);
-	//d0.y = ((h[0][2] + 2.0 * h[1][2] + h[2][2]) - (h[0][0] + 2.0 * h[1][0] + h[2][0])) / (8.0 * Scale.z);
-
-	//d1.x = (h[0][0] + 2.0 * h[0][1] + h[0][2] + h[2][0] + 2.0 * h[2][1] + h[2][2] - (2.0 * h[1][0] + 4.0 * h[1][1] + 2.0 * h[1][2])) / (4.0 * Scale.x * Scale.x);
-	//d1.y = (h[0][0] + 2.0 * h[1][0] + h[2][0] + h[0][2] + 2.0 * h[1][2] + h[2][2] - (2.0 * h[0][1] + 4.0 * h[1][1] + 2.0 * h[2][1])) / (4.0 * Scale.z * Scale.z);
-	//d1.z = (h[0][2] + h[2][0] - h[0][0] - h[2][2]) / (4.0 * Scale.x * Scale.z);
+	d1.x = (r - 2.0 * c + l) / (Scale.x * Scale.x);
+	d1.y = (t - 2.0 * c + b) / (Scale.z * Scale.z);
+    d1.z = (tr - br - tl + bl) / (4.0 * Scale.x * Scale.z);
 }
 
 #endif
